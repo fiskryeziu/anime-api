6c5922812b5e81e1732d6b99c56c41ee093948f9
fiskryeziu <fiskryeziu@gmail.com>
2024-09-18 17:41:55 +0200

    add trending, new, upcoming anime data

--- src/services/animeScraper.ts
+++ src/services/animeScraper.ts
@@ -1,14 +1,8 @@
 import * as cheerio from 'cheerio';
 
-// All the scraper functions we'll be either here
-// or multiple files in services folder
-
-
 const scrapeAnime = async () => {
     try {
-        // scrape logic goes here 
         const $ = await cheerio.fromURL('https://hianime.to/home');
-
 
         const data = $.extract({
             spotlight: [
@@ -24,8 +18,8 @@ const scrapeAnime = async () => {
                             return $(this).text().trim();
                         }).get();
 
-                        const episodes = $(el).find('.sc-detail .tick-item').eq(0).text()
-                        const captions = $(el).find('.sc-detail .tick-item').eq(1).text()
+                        const captions = $(el).find('.sc-detail .tick-item').eq(0).text()
+                        const dubEp = $(el).find('.sc-detail .tick-item').eq(1).text()
 
 
                         const details = {
@@ -33,13 +27,92 @@ const scrapeAnime = async () => {
                             duration: detailsData[1] || '',
                             date: detailsData[2] || '',
                             quality: detailsData[3] || '',
-                            episodes: episodes || '',
-                            captions: captions || ''
+                            captions: captions || '',
+                            dubEp: dubEp || ''
                         };
                         return { imgSrc: img, spotlightRanking: spotlightText, details }
                     }
                 }
-            ]
+            ],
+            trending: [
+                {
+                    selector: '#anime-trending .trending-list .swiper-wrapper .swiper-slide',
+                    value: (el) => {
+                        const img = $(el).find('img').attr('data-src')
+                        const trendingRank = $(el).find('.item .number span').text()
+
+                        return { img, trendingRank }
+                    }
+                },
+            ],
+            latestEp: [
+                {
+                    selector: '.film_list-wrap:first .flw-item',
+                    value: (el) => {
+                        const img = $(el).find('img').attr('data-src')
+                        const latestEpisodeUrl = 'https://hianime.to' + $(el).find('a').attr('href')
+                        const animeUrl = 'https://hianime.to' + $(el).find('.film-name a').attr('href')
+                        const url = { latestEpisodeUrl, animeUrl }
+
+                        const name = $(el).find('.film-name').text()
+                        const type = $(el).find('.film-detail .fd-infor span:first').text()
+                        const duration = $(el).find('.fdi-duration').text()
+                        const captions = $(el).find('.film-poster .tick-sub').text()
+                        const dubEp = $(el).find('.film-poster .tick-dub').text()
+                        const episodes = $(el).find('.film-poster .tick-eps').text()
+                        const details = { name, type, duration, captions, dubEp, episodes }
+
+                        return { img, url, details }
+                    },
+                }
+
+            ],
+            newOnZoro: [
+                {
+                    selector: '.film_list-wrap:eq(1) .flw-item',
+                    value: (el) => {
+                        const img = $(el).find('img').attr('data-src')
+                        const latestEpisodeUrl = 'https://hianime.to' + $(el).find('a').attr('href')
+                        const animeUrl = 'https://hianime.to' + $(el).find('.film-name a').attr('href')
+                        const url = { latestEpisodeUrl, animeUrl }
+
+                        const name = $(el).find('.film-name').text()
+                        const type = $(el).find('.film-detail .fd-infor span:first').text()
+                        const duration = $(el).find('.fdi-duration').text()
+                        const captions = $(el).find('.film-poster .tick-sub').text()
+                        const episodes = $(el).find('.film-poster .tick-dub').text()
+                        const details = { name, type, duration, captions, episodes }
+
+                        return { img, url, details }
+
+                    },
+                }
+
+            ],
+            topUpComing: [
+                {
+                    selector: '.film_list-wrap:eq(2) .flw-item',
+                    value: (el) => {
+                        const img = $(el).find('img').attr('data-src')
+                        const latestEpisodeUrl = 'https://hianime.to' + $(el).find('a').attr('href')
+                        const animeUrl = 'https://hianime.to' + $(el).find('.film-name a').attr('href')
+                        const url = { latestEpisodeUrl, animeUrl }
+
+                        const name = $(el).find('.film-name').text()
+                        const type = $(el).find('.film-detail .fd-infor span:first').text()
+                        const startDate = $(el).find('.film-detail .fd-infor .fdi-duration').text()
+                        const details = { name, type, startDate, }
+
+                        return { img, url, details }
+                    },
+                }
+            ],
+            // schedule: [
+            //     {
+            //         selector: '',
+            //         value: () => { }
+            //     }
+            // ]
 
         })
 
