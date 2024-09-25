e65f7f00bb66aa41865c48577b58083739a34034
fiskryeziu <fiskryeziu@gmail.com>
2024-09-25 21:10:53 +0200

    add: morgan & caching functionality

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-import express, { urlencoded, json } from "express";
+import express, { urlencoded, json, Request, Response, NextFunction } from "express";
+import morgan from 'morgan'
 import animeRoute from './routes/animeRoute'
 import { errorHandler, notFound } from "./middlewares/errorMiddleware";
+import { cacheMiddleware } from "./middlewares/cacheMiddleware";
 
 const port = process.env.PORT || 5000
 const app = express();
 
+
 app.use(urlencoded({ extended: true }));
 app.use(json());
+app.use(morgan('dev'))
+
+
 
 app.get("/", (req, res) => {
     res.status(200).json({ msg: "Server is up and running" });
 });
 
-app.use('/api/anime', animeRoute)
+
+app.use('/api/anime', cacheMiddleware, animeRoute)
 
 app.use(notFound)
 app.use(errorHandler)
